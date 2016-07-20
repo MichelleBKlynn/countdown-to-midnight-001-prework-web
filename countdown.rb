8b3a2f81f6c55a179e770592e0d468ee99c10745
MichelleBKlynn <michellestaton@gmail.com>
2016-07-20 14:48:04 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
-#write your code here
 
-def countdown
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+    "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(seconds_to_midnight)
+seconds_to_midnight = 10
+  while seconds_to_midnight < 11
+    puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -= 1 
+        sleep(1)
+    if seconds_to_midnight == 0
+break
+ 
+ 
+end
+
+end
+end
+
+
